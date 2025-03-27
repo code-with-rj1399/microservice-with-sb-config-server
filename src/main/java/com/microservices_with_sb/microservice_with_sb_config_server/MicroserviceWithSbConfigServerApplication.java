28f6077eeb2404d80769fe98c3e639af00ecc615
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-27 13:11:35 +0530

    ZIPKIN configured for deparments service

--- src/main/java/com/microservices_with_sb/microservice_with_sb_config_server/MicroserviceWithSbConfigServerApplication.java
+++ src/main/java/com/microservices_with_sb/microservice_with_sb_config_server/MicroserviceWithSbConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.microservices_with_sb.microservice_with_sb_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MicroserviceWithSbConfigServerApplication {
 
 	public static void main(String[] args) {
