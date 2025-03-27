e0d235e4d820174b5f418079c4bee7321a507dee
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-27 12:52:15 +0530

    intial commit

---
+++ src/main/java/com/microservices_with_sb/microservice_with_sb_config_server/MicroserviceWithSbConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservices_with_sb.microservice_with_sb_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWithSbConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceWithSbConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices_with_sb/microservice_with_sb_config_server/MicroserviceWithSbConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices_with_sb.microservice_with_sb_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWithSbConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
