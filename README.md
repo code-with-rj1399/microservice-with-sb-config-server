4c84345cbc08739bc14d12162649779896d1c664
Rajesh <rajesh4.ramesh@gmail.com>
2025-03-28 13:05:10 +0530

    refactored

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # microservice-with-sb-config-server
+
+# staring ZIPKIN 
+`docker run -d -p 9411:9411 openzipkin/zipkin` <br/>
+Now you can access it http://localhost:9411/zipkin/ 
--- src/main/java/com/microservices_with_sb/microservice_with_sb_config_server/Application.java
+++ src/main/java/com/microservices_with_sb/microservice_with_sb_config_server/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class MicroserviceWithSbConfigServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceWithSbConfigServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
